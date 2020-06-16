a7679a80502782633261a4420e92dfe2454abd1e
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-16 11:33:43 -0400

    added css styling for all compnents

--- src/components/Issue.jsx
+++ src/components/Issue.jsx
@@ -5,6 +5,25 @@ import 'bootstrap/dist/css/bootstrap.css'
 import { Button, Col, Row, Form, } from 'react-bootstrap'
 
 
+function getUserLink(user) {
+  return <a href={user.url}>{user.login}</a>
+}
+
+function getRelativeTime(time) {
+  return moment.utc(time).fromNow();
+}
+
+
+function getMetaFragment(issue) {
+  const { number, state, created_at, closed_at, user } = issue;
+
+  if (state === 'open') {
+    return <>#{ number} opened { getRelativeTime(created_at)} by { getUserLink(user)}</>;
+  } else {
+    return <>#{ number} was closed by { getUserLink(user)} {getRelativeTime(closed_at)}</>
+  }
+}
+
 function Issue(props) {
   const { issue } = props;
 
