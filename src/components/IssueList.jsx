be928217014ce85f3aac0674e09de2add3949c13
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-16 11:11:01 -0400

    initial commit

---
+++ src/components/IssueList.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './IssueList.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button, Col, Row, Form, } from 'react-bootstrap'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: [],
+      editMe: [],
+    }
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <h1>Github Issues</h1>
+        </header>
+
+      </div>
+    );
+  }
+}
+
+export default App;
