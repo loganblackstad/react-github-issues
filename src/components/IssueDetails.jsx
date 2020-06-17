9a112df7655ea3f62960d348627bd80ed4223909
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-17 19:16:08 -0400

    commit

--- src/components/IssueDetails.jsx
+++ src/components/IssueDetails.jsx
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
-import Issue from './Issue.jsx'
-import moment from 'moment';
+import React, { Component } from 'react'
 import ReactMarkdown from "react-markdown/with-html";
 import 'bootstrap/dist/css/bootstrap.css'
+import '../App.css';
+import { Container } from 'react-bootstrap'
+import open from './open.svg'
+import moment from 'moment';
 
 
 export default class IssueDetail extends Component {
@@ -37,10 +39,37 @@ export default class IssueDetail extends Component {
     moment().format();
 
     return (
-      <div className="IssueDetail" >
-        <ReactMarkdown source={body} escapeHtml={false} />
-        {/* {this.state.issues.map(issue => <Issue issue={issue} key={issue.id} />)} */}
+      // <div className="IssueDetail" >
+      //   <ReactMarkdown source={body} escapeHtml={false} />
+      //   {this.state.issues.map(issue => <Issue issue={issue} key={issue.id} />)}
+      // </div>
+      <div>
+        <Container>
+          <div className="issueHeader">
+            <h1>{title} <span style={{ color: '#999' }}>#{number}</span></h1>
+            <div className="openIssue">
+              <span className="openIcon">
+                <img src={open}></img> Open
+              </span>
+              <span className="issueInfo">
+                <a href={user.html_url} className="userName">{user.login}</a> opened this issue {moment(updated_at).fromNow()} - {comments ? comments : comments} comments
+              </span>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className="issueBody border">
+            <div className="bodyHeader">
+              <a href={user.html_url} className="userName">{user.login}</a> commented {moment(updated_at).fromNow()}
+            </div>
+            <div className="bodyMain">
+              <ReactMarkdown source={body} escapeHtml={false} />
+            </div>
+          </div>
+        </Container>
       </div>
+
     );
   }
 }
